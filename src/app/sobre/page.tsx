4b7d5747b43b2e1c1c38f3c743407a82b4fba383
Marcio-Brust <brustmw21@gmail.com>
2024-05-09 23:09:07 -0300

    ssr

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,14 +1,12 @@
-import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
-export const metadata: Metadata = {
-  title: "Sobre",
-  description: "Essa é a página sobre",
-};
+const Width = dynamic(() => import("@/component/width"), { ssr: false });
 
 export default function SobrePage() {
   return (
     <main>
       <h2>Sobre</h2>
+      <Width />
     </main>
   );
 }
